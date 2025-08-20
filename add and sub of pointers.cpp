29bb6a2ff430254dbdffc4f32907e8de67ca269a
Prathamesh <80304812+PageNotFound222@users.noreply.github.com>
2025-08-21 02:04:02 +0530

    Create add and sub of pointers.cpp

---
+++ add and sub of pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+    int b=5;
+    int*a1;
+    a1=&a;
+    int*b1;
+    b1=&b 
+    int sum,diff;
+    sum=*a1+*b1;
+    diff=*a1-*b1;
+    cout<<"Sum is: "<<sum<<endl;
+    cout<<"Differe+nce is: "<<diff<<endl;
+
+}
+/*
+Output:
+Sum is: 15
+Differe+nce is: 5
+*/
